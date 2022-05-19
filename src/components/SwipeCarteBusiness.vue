664bc2e984b06c1d4de0be549362d89b95c59328
MEREL ANNE p1905532 <anne.merel@etu.univ-lyon1.fr>
2022-05-19 16:34:55 +0200

    end of day save

--- src/components/SwipeCarteBusiness.vue
+++ src/components/SwipeCarteBusiness.vue
@@ -6,8 +6,19 @@ export default {
     data() {
         return {
             name: 'Test_naam',
-            
-    }
+            s_left: false,
+            s_right: false,
+
+
+        }
+    },
+    methods: {
+        swipe_left() {
+            this.s_left = !this.s_left
+        },
+        swipe_right() {
+            this.s_right = !this.s_right
+        }
     }
 }
 
@@ -18,12 +29,42 @@ export default {
         <h3> {{ name }} </h3>
         <div>
             <PieChart></PieChart>
+            <button class="swipe-left" @click="swipe_left">LEFT</button>
+            <button class="swipe-right" @click="swipe_right">RIGHT</button>
+            <p> {{ s_left }} en {{s_right}} </p>
+
+            <div v-if = "s_right">
+                <h3>FEEDBACK????</h3>
+            </div>
+
         </div>
     </div>
 </template>
 
 
 <style>
+.swipe-left {
+    background-color: green;
+    border-color: green;
+    align-self: left;
+}
+
+.swipe-left:hover {
+    background-color: whitesmoke;
+    border-color: green;
+}
+
+.swipe-right {
+    background-color: red;
+    border-color: red;
+    align-self: right;
+}
+
+.swipe-right:hover {
+    background-color: whitesmoke;
+    border-color: red;
+}
+
 .swipeCartBusiness {
     background-color: aliceblue;
 
--- src/views/Swipe.vue
+++ src/views/Swipe.vue
@@ -11,11 +11,15 @@
 <template>
     <div class = "swipe">
         <h3> The swipe page </h3>
+        <p> kleine algemene uitleg : Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <SwipeCarteBusiness></SwipeCarteBusiness>
         
     </div>
 
-
+    <!-- carousel idee voor kaarten https://vuejsexamples.com/10-vue-carousel-code-examples/ -->
+    <!-- nog beter : https://swiperjs.com/demos   infinite loop --> 
 </template>
 <style>
 
