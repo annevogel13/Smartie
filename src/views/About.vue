f969e7efdb19cb5b725eee192f085066924b140a
MEREL ANNE p1905532 <anne.merel@etu.univ-lyon1.fr>
2022-05-19 09:20:02 +0200

    routing met Home/Profile/About werkt

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class = "about">
+        <h1>About</h1>
+        <p>tes test test </p>
+    </div>
+
+</template>
---
+++ src/views/Profile.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="profile" >
+        <h4>Bonjour</h4>
+        <p> test test test </p>
+    </div>
+</template>
