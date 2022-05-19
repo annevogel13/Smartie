552a06c1bc77f17183bd8753230825f0e254bfb6
MEREL ANNE p1905532 <anne.merel@etu.univ-lyon1.fr>
2022-05-19 16:10:28 +0200

    piechart toegevoegd

---
+++ src/components/PieChart.vue
@@ -0,0 +1,40 @@
+<script>
+
+import VueApexCharts from 'vue3-apexcharts'
+
+export default {
+    data() {
+        return {
+            series: [14, 23, 21, 17, 15, 10, 12, 17, 21],
+            chartOptions: {
+                chart: {
+                    type: 'polarArea',
+                },
+                stroke: {
+                    colors: ['#fff']
+                },
+                fill: {
+                    opacity: 0.8
+                },
+                responsive: [{
+                    breakpoint: 300,
+                    options: {
+                        chart: {
+                            width: 100
+                        },
+                        legend: {
+                            position: 'bottom'
+                        }
+                    }
+                }]
+            },
+            components: { VueApexCharts }
+        }
+    }
+}
+</script>
+<template>
+    <div class="pieChart">
+            <apexchart type="polarArea" :options="chartOptions" :series="series"></apexchart>
+        </div>
+</template>
