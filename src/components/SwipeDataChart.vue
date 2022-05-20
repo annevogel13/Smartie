38b307f803b12767a5413a7e05ac27172e4f19d3
MEREL ANNE p1905532 <anne.merel@etu.univ-lyon1.fr>
2022-05-20 13:41:14 +0200

    dashboard met grafiekje

---
+++ src/components/SwipeDataChart.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    data() {
+        return {
+            series: [{
+                data: [34, 44, 54, 21, 12, 43, 33, 23, 66, 66, 58]
+            }],
+            chartOptions: {
+                chart: {
+                    type: 'line',
+                    height: 350
+                },
+                stroke: {
+                    curve: 'stepline',
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                title: {
+                    text: 'Statitieken ',
+                    align: 'center'
+                },
+                markers: {
+                    hover: {
+                        sizeOffset: 4
+                    }
+                }
+            },
+        }
+    }
+}
+
+</script>
+<template>
+    <div id="chart">
+        <apexchart type="line" height="350" :options="chartOptions" :series="series"></apexchart>
+    </div>
+</template>
+<style>
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,14 @@
 <script>
-    export default {
-        data(){
-            return {
-                role : "bedrijf", 
-            }
+
+import SwipeDataChart from "../components/SwipeDataChart.vue"
+export default {
+    data() {
+        return {
+            role: "bedrijf",
         }
-    }
+    },
+    components : { SwipeDataChart}
+}
 </script>
 
 <template>
@@ -13,19 +16,40 @@
 
         <h3> Dashboard </h3>
         <p>(gepersonaliseerd --> twee verschillende versies)</p>
-        <label>Role : </label>
-       
-            <select v-model="role">
-                <option value="bedrijf">Bedrijf</option>
-                <option value="cursist">Cursist</option>
-            </select>
-     
+
+        <select class="quick" v-model="role">
+            <option value="bedrijf">Bedrijf</option>
+            <option value="cursist">Cursist</option>
+        </select>
+
+        <div v-if = "role == 'cursist'">
+            <h3> Pagina voor cursisten </h3>
+        </div>
+
+        <div v-if = "role == 'bedrijf'">
+            <h3> Pagina voor bedrijven </h3>
+            <div class = "swipeData">
+
+                <SwipeDataChart></SwipeDataChart>
+            </div>
+        </div>
     </div>
 
 </template>
 
 <style>
+
+.swipeData {
+    padding : 30px; 
+    float : right; 
+    width : 50%; 
+}
+
 .dashboard {
     width: 1000px;
+}
+
+.quick {
+    width: 100px;
 }
 </style>
