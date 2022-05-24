14d216fe614796bb04a72c6e4f5d7d9438fb39d3
MEREL ANNE p1905532 <anne.merel@etu.univ-lyon1.fr>
2022-05-24 15:18:31 +0200

    verbetert na suggesties van eslint

---
+++ src/views/ProfileUser.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="profileUser" >
+        <h4>Profile</h4>
+        <p>Hier kan je je profiel aanmaken</p>
+        <signup-form />
+    </div>
+</template>
+<script>
+    
+    import SignupForm from "../components/SignupForm.vue"
+
+    export default {
+        name : 'ProfileUser',
+        components : { SignupForm }
+    }
+    
+</script>
+<style scoped >
+.profile{
+    width :100%; 
+}
+</style>
---
+++ src/views/RegisterUser.vue
@@ -0,0 +1,120 @@
+<script>
+
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+import { db } from '../db'
+
+export default {
+    data() {
+        return {
+            email: "",
+            password: "",
+            role: "",
+            login: false,
+            newUser : ""
+        }
+    },
+    methods: {
+        register() {
+            console.log("Register new user")
+            const auth = getAuth();
+            createUserWithEmailAndPassword(auth, this.email, this.password)
+                .then(() => {
+                    this.$store.state.user.UID = auth.currentUser.uid;
+                })
+                .then(this.$router.push('./DashboardUser'))
+                .catch((error) => {
+                    const errorMessage = error.message;
+                    console.log(errorMessage)
+                })
+            this.create_user_db();
+        },
+
+        login_user() {
+            const auth = getAuth();
+            signInWithEmailAndPassword(auth, this.email, this.password)
+                .then(() => {//(userCredential) => {
+                    console.log("Ingelogged")
+                    //const user = userCredential.user;
+                    this.$store.state.user.UID = auth.currentUser.uid;
+
+                })
+                .then(this.$router.push('./DashboardUser'))
+                .catch((error) => {
+
+                    const errorMessage = error.message;
+                    console.log(errorMessage)
+                })
+        },
+        async addUser(){
+            if(this.newUser) {
+                await db.collection("users").add({name : this.newUser})
+                
+            }
+        },
+
+    },
+
+    components: {}
+}
+
+</script>
+<template>
+    <div class="registerUser">
+
+        <div v-if='!login'>
+            <h3>Creer een account</h3>
+            <input type="text" v-model="email" placeholder="Email">
+            <br>
+            <input type="password" v-model="password" placeholder="Password">
+            <br>
+            <select class="role" v-model="role" placeholder="Bedrijf">
+                <option value="bedrijf">Bedrijf</option>
+                <option value="cursist">Cursist</option>
+            </select>
+            <br>
+            <button @click="register()">
+                Registeren
+            </button>
+            <button @click="this.login = !login">Wisselen naar login</button>
+        </div>
+        <div v-if='login'>
+            <h3>Login</h3>
+            <input type="text" v-model="email" placeholder="Email">
+            <br>
+            <input type="password" v-model="password" placeholder="Password">
+            <br>
+            <button @click="login_user()">
+                Login
+            </button>
+            <button @click="this.login = !login">Wisselen naar registeren</button>
+        </div>
+
+    </div>
+</template>
+<style scoped>
+.registerUser {
+    margin-top: 40px;
+    width: 1000px;
+    vertical-align: center;
+}
+
+input,
+.role {
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+
+}
+
+button {
+    margin: 10px;
+    background-color: aliceblue;
+    border: aliceblue;
+    padding: 5px;
+
+}
+</style>
