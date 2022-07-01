5cbb8ed10a5e71771f58a2c21375caed78990363
MEREL ANNE p1905532 <anne.merel@etu.univ-lyon1.fr>
2022-07-01 14:11:43 +0200

    grafiek kernwoorden bug opgelost

--- src/components/Charts/LineChart.vue
+++ src/components/Charts/LineChart.vue
@@ -2,20 +2,17 @@
 
 import VueApexCharts from 'vue3-apexcharts'
 
-
-
 export default {
     data() {
         return {
             series: [{
-
                 data: [
-                    this.$store.state.user.data_to_be_displayed.questionnaire.k1,
-                    this.$store.state.user.data_to_be_displayed.questionnaire.k2,
-                    this.$store.state.user.data_to_be_displayed.questionnaire.k3,
-                    this.$store.state.user.data_to_be_displayed.questionnaire.k4,
-                    this.$store.state.user.data_to_be_displayed.questionnaire.k5,
 
+                    this.$store.state.user.data_to_be_displayed.questionnaire.k1 + 1, // hierarchisch 
+                    this.$store.state.user.data_to_be_displayed.questionnaire.k2 + 1, // thuiswerken 
+                    this.$store.state.user.data_to_be_displayed.questionnaire.k4 + 1, // vrijmibo 
+                    this.$store.state.user.data_to_be_displayed.questionnaire.k5 + 1, // muziek 
+                    this.$store.state.user.data_to_be_displayed.questionnaire.s1 + 1, // intercollegiaal 
                 ],
             }],
             chartOptions: {
@@ -23,13 +20,17 @@ export default {
                     height: 350,
                     type: 'radar',
                 },
-                labels : ['Hiërarchisch', 'Thuiswerken', 'Vrijmibo', 'Muziek', 'Kernwoord5'], 
+                labels: ['Flexibele werktijden', 'Meedenken', 'Geek', 'Intercollegiaal', "Thuiswerken"],
                 title: {
                     text: 'Grafiek van kernwoorden'
                 },
                 xaxis: {
-                    categories: ['Hiërarchisch', 'Thuiswerken', 'Vrijmibo', 'Muziek', 'Kernwoord5']
-                }
+                    categories: ['Flexibele werktijden', 'Meedenken', 'Geek', 'Intercollegiaal', "Thuiswerken"]
+                },
+                yaxis : {
+                    show : false , 
+                    max : 101, 
+                } 
             },
             components: { VueApexCharts },
 
@@ -39,7 +40,6 @@ export default {
 }
 </script>
 <template>
-
     <div id="chart">
         <apexchart type="radar" height="350" :options="chartOptions" :series="series"></apexchart>
     </div>
